5b8cde3331c7ff24ca3b03514160c0bd5e41317b
Vishnu Shanker <brvishnu92@gmail.com>
2024-03-26 22:55:19 +1100

    f

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -4,6 +4,6 @@ export const getBankBalance = async (req, res) => {
 
     const customers = getExistingCustomers()
     const bankBalance = customers.reduce((amt, { balance }) => parseFloat(amt) + parseFloat(balance), 0)
-    res.status(200).json({ balance: bankBalance, customers })
+    res.status(200).json({ balance: parseFloat(bankBalance.toFixed(2)), customers })
 
 }
